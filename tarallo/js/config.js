668113d05b58deb4e287545cadb3debe434ee351
Thiago Vilaça <thevilaca@gmail.com>
2016-07-20 04:06:07 -0300

    site tarallo tattoo

---
+++ tarallo/js/config.js
@@ -0,0 +1,30 @@
+// Configuration Object
+var config = {};
+
+/*-------------------------------------------------------------------*/
+/*      1. Form Messages
+/*-------------------------------------------------------------------*/
+
+config.formMessages = {
+    sent          : 'Message sent successfully. Thanks!',
+    fail          : 'Oops! Sending error. Please try again',
+    failEmail     : 'Invalid Email!',
+    emptyFields   : 'Complete all the required fields!!'
+};
+
+/*-------------------------------------------------------------------*/
+/*      2. Google Map
+/*-------------------------------------------------------------------*/
+
+config.gmap = {
+    latitude    : '-23.640260',
+    longitude   : '-46.697402',
+    zoom        : 15,
+    marker      : 'img/marker.png'
+};
+
+/*-------------------------------------------------------------------*/
+/*      3. Easy Background System Settings
+/*-------------------------------------------------------------------*/
+
+config.backgrouns = {};
---
+++ tarallo/mailer/config.php
@@ -0,0 +1,22 @@
+<?php
+
+/* =========================================== MAIN RECIPIENT  =========================================== */
+
+$to = 'your_email@domain.com';						// TODO: Replace with your main email
+
+/* ========================================= APPOINTMENT FORM ============================================ */
+
+$send_to_artist = false;							// Set TRUE to send the message also to the selected artist
+$artists_email = array(
+	//'Artist A' => 'artist_a@domain.com',
+    //'Artist B' => 'artist_b@domain.com',
+	);
+
+/* ============================================ SMTP SETTINGS ============================================ */
+
+$smtp = false;							// Set TRUE if you want use a custom smtp server
+$smtp_username = '';					// Add your smtp username
+$smtp_password = '';					// Add your smtp password
+$smtp_host = '';						// Set the hostname of the mail server (ex. smtp.gmail.com)
+$smtp_port = '';						// Set the SMTP port number - likely to be 25, 465 or 587
+$smtp_secure = '';						// Set the encryption system to use. Accepted values: 'ssl' or 'tls'
